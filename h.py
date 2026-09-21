d6dcd1cfe24a770f9ee30f98238bc84b13162a48
kimchi <kimsdev12@gmail.com>
2026-09-21 22:52:38 +0800

    refined short-movie filtering and tidy analysis script

--- h.py
+++ h.py
@@ -1,19 +1,23 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-data = 'datasets/netflix_data.csv'
-netflix_df = pd.read_csv(data)
-#print(netflix_df.head())
+# Load the dataset.
+data_path = 'datasets/netflix_data.csv'
+netflix_df = pd.read_csv(data_path)
 
-#displaying only year 1990 and above
+# Keep titles released in 1990 or later.
 netflix_1990_and_above = netflix_df[netflix_df['release_year'] >= 1990]
-#highest duration to low
-netflix_frequent_duration = netflix_1990_and_above.sort_values('duration', ascending=False) 
-#select only relevant columns       
-netflix_selected_columns = netflix_frequent_duration[['title','country','release_year','duration']]
 
-#count the short movies that are less than 90 mins
-short_movies_count = netflix_selected_columns[netflix_selected_columns['duration']<90]
-#print(netflix_selected_columns.head(10))
-print(short_movies_count.head(20))
+# Keep movies with a duration under 90 minutes.
+short_movies = netflix_1990_and_above[
+    (netflix_1990_and_above['type'] == 'Movie')
+    & (netflix_1990_and_above['duration'] < 90)
+]
+
+selected_columns = ['title', 'type', 'country', 'release_year', 'duration']
+short_movies = short_movies[selected_columns]
+short_movies = short_movies.sort_values('duration', ascending=False)
+short_movies_count = len(short_movies)
+
+print(short_movies.head(20))
+print(f'short movies released in 1990 or later: {short_movies_count}')
