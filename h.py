824675a75c38260f07088f774d5e6068d9610b78
kimchi <kimsdev12@gmail.com>
2026-09-21 23:23:20 +0800

    visualize duration distribution of short movies released since 1990 using histogram

--- h.py
+++ h.py
@@ -19,5 +19,19 @@ short_movies = short_movies[selected_columns]
 short_movies = short_movies.sort_values('duration', ascending=False)
 short_movies_count = len(short_movies)
 
-print(short_movies.head(20))
+#print(short_movies.head(20))
 print(f'short movies released in 1990 or later: {short_movies_count}')
+
+#using histogram
+plt.hist(
+    short_movies['duration'],
+    bins=10,
+    edgecolor='black'
+)
+plt.title('NETFLIX SHORT-MOVIES RELEASED IN 1990 OR LATER ')
+plt.xlabel('duration (in minutes)')
+plt.ylabel('number of movies')
+plt.show()
+
+# Most movies in this filtered dataset have durations between ___ and ___ minutes.
+# Movies below ___ minutes appear less/more frequently than movies near 90 minutes.
